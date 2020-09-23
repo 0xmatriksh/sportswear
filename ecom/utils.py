c4fc9078eae47dc51b1ff7e4482d3488533fe15e
kishmat <kishmat17@gmail.com>
2020-09-23 13:26:16 +0545

    error solved

--- ecom/utils.py
+++ ecom/utils.py
@@ -1,11 +1,10 @@
 from .models import *
 
 def cartData(request):
-    if request.user.is_authenticated:
         customer = request.user.customer
         order,created = Order.objects.get_or_create(customer=customer, complete=False)
         items = order.orderitem_set.all()
         cartItems = order.get_cart_items
-    else:
-        print("User is not authenticated.")
-    return {'cartItems':cartItems, 'order':order,'items':items}
+        return {'cartItems':cartItems, 'order':order,'items':items}
+   
+    
